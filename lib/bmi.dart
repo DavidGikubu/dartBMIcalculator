8566a4352544ccaf21d818f057f4a540281e91ed
Gikubu <davidndungu8203@gmail.com>
2024-03-05 02:15:21 +0300

    here i create a function and display the users bmi

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -4,6 +4,12 @@ void main() {
   print("welcome to BMI  calculator!");
   print("enter your height in meters: \n");
   double? height = double.parse(stdin.readLineSync()!);
-  print("enter your weight in Kgs: ")
-  int? weight = int.parse(stdin.readLineSync()!);
+  print("enter your weight in Kgs: \n");
+  double? weight = double.parse(stdin.readLineSync()!);
+  double bodyMassIndex = calculateBMI(height, weight);
+  print("your BMI is: $bodyMassIndex");
+}
+
+double calculateBMI(double height, double weight) {
+  return weight / (height * height);
 }
